89dc4dff591f3bb47af2b76cde4a2a5a34c90a85
HP 6th <iqra615>
2020-06-18 08:42:44 -0700

    Face

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,17 @@
+package come.codewithiqra;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] array = new String[5];
+
+	array[0] = "+\\\"\\\"\\\"\\\"\\\"+";
+	array[1] = "[| o o |]";
+	array[2] = "|  ^  |";
+    array[3] = "| '-' |";
+    array[4] = "+-----+";
+
+        for(int i=0; i<5; i++)
+            System.out.println(array[i]);
+    }
+}
